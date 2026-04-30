f7cb35f4c676278b2a1590d24a87fceb76c581e3
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-29 23:14:14 -0300

    feat: initial app creating

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("server running");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ body: "Welcome to the SubTrack API" });
+});
+
+app.listen({
+  port: 3000,
+  hostname: () => {
+    console.log("SubTrack API is running on http://localhost:3000");
+  },
+});
+
+export default app;
