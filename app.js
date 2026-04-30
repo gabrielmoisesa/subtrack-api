e6ccad519755e326c0be236309d069961beaaa0f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-29 23:37:49 -0300

    feat: install and setup dotenv, env and port variables

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the SubTrack API");
+  res.send("Welcome to the SubTrack API!");
 });
 
-app.listen(3000, () => {
-  console.log("SubTrack API is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`SubTrack API is running on http://localhost:${PORT}`);
 });
 
 export default app;
